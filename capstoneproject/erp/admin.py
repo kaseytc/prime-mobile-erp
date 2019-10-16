8665295349e92fe51913a7999a72c2d528fe7f3c
kaseytc <16894195+kaseytc@users.noreply.github.com>
2019-10-16 16:53:25 +0800

    Customize admin pages

--- capstoneproject/erp/admin.py
+++ capstoneproject/erp/admin.py
@@ -1,20 +1,55 @@
 from django.contrib import admin
-
-# Register your models here.
 from erp.models import Accounts, Customers, Employees, Inventory, Invoices, Orders
 
-class InventoryAdmin(admin.ModelAdmin):
-    list_display = ('make', 'model', 'inv_price', 'quantity')
+# Register your models here.
+
+
+class AccountAdmin(admin.ModelAdmin):
+    model = Accounts
+    list_display = ('username', 'acct_type', 'get_name')
+
+    def get_name(self, obj):
+        return obj.emp.fname + ' ' + obj.emp.lname
+    get_name.short_description = 'Employee Name'
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('fname', 'lname', 'dob')
+
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('emp_id', 'fname', 'lname', 'title' )
+    list_display = ('fname', 'lname', 'title' )
 
 
-admin.site.register(Accounts)
-admin.site.register(Customers)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('make', 'model', 'bin_bay', 'bin_aisle', 'inv_price', 'quantity')
+
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('invoice_id', 'invoice_dt', 'pay_type', 'invoice_num', 'get_name')
+
+    def get_name(self, obj):
+        return obj.emp.fname + ' ' + obj.emp.lname
+    get_name.short_description = 'Employee Name'
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'order_dt', 'status', 'quantity', 'get_inventory', 'get_invoice')
+
+    def get_inventory(self, obj):
+        return obj.inventory.make + ' ' + obj.inventory.model
+    get_inventory.short_description = 'Make & Model'
+
+    def get_invoice(self, obj):
+        return obj.invoice.invoice_id
+    get_invoice.short_description = 'Invoice ID'
+
+
+admin.site.register(Accounts, AccountAdmin)
+admin.site.register(Customers, CustomerAdmin)
 admin.site.register(Employees, EmployeeAdmin)
 admin.site.register(Inventory, InventoryAdmin)
-admin.site.register(Invoices)
-admin.site.register(Orders)
+admin.site.register(Invoices, InvoiceAdmin)
+admin.site.register(Orders, OrderAdmin)
 
 
